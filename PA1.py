539e010942646b38181ef004afa537eb812b8c61
Abdulelah Hajjar <abdulelahhajjar@gmail.com>
2021-02-20 15:38:55 +0300

    Im equal method in Node class; Implement generateNextStatesForAQueen; Implemented astar function

--- PA1.py
+++ PA1.py
@@ -2,6 +2,11 @@ import random
 import math
 from collections import defaultdict
 from queue import PriorityQueue
+import time
+import copy
+from itertools import *
+import functools
+import heapq
 
 N = 8
 
@@ -12,7 +17,7 @@ def generateBoard(N):
     # In my implementation of the board, 0 == space on the board with no queen. 1 == space on the board with one queen.
 
     # Create (N^2 - N) elements (spaces) in a list with NO queens, and join it with N elements with all queens. (List size = N^2 - N + N = N^2)
-    board = [0]*(8**2 - 8) + [1]*(8)
+    board = [0]*(N**2 - N) + [1]*(N)
 
     # Shuffle the array for the initial state
     random.shuffle(board)
@@ -20,7 +25,7 @@ def generateBoard(N):
     # Convert array to 2D by slicing it into rows
     board2D = []
     for i in range(0, N**2, N):
-        board2D.append(board[i:i+8])
+        board2D.append(board[i:i+N])
 
     # return board2D
     return [[0, 0, 0, 0, 0, 0, 0, 0],
@@ -36,6 +41,8 @@ def generateBoard(N):
 def ncr(n, r):
     if r > n:
         return 0
+    elif r == n:
+        return 1
     else:
         return int(math.factorial(n) / (math.factorial(r) * math.factorial(n - r)))
 
@@ -86,31 +93,113 @@ def drawBoard(board):
 
 
 class Node():
-    def __init__(self, parentNode=None, board=None):
-        self.parentNode = parentNode
+    def __init__(self, board=None):
         self.board = board
         self.gn = 0
         self.hn = 0
-        self.fn = 0
+
+    def fn(self):
+        return self.gn + self.hn
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            if functools.reduce(lambda i, j: i and j, map(lambda m, k: m == k, self.board, other.board), True):
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def __lt__(self, other):
+        return self.fn() < other.fn()
+
+
+def generateNextStatesForQueenAt(i, j, board):
+    nextStates = []
+    for xOffset in [-1, 0, 1]:
+        for yOffset in [-1, 0, 1]:
+            if xOffset == 0 and yOffset == 0:
+                continue
+
+            newI = i + xOffset
+            newJ = j + yOffset
+
+            if newI < 0 or newJ < 0 or newI >= N or newJ >= N:
+                continue
+
+            if board[newI][newJ] == 1:
+                continue
+
+            tempBoard = copy.deepcopy(board)
+            tempBoard[i][j] = 0
+            tempBoard[newI][newJ] = 1
+
+            node = Node(tempBoard)
+            nextStates.append(node)
+    return nextStates
 
 
 def astar(board):
-    root = Node(None, board)
-
-    fringe = PriorityQueue()
+    # TODO: Explain gn and hn
+    fringe = []
     visited = []
 
-    fringe.put((calculateHeuristic(board), board))
-    while not fringe.empty():
-        currentNode = fringe.get()[1]
+    root = Node(board)
+    root.hn = calculateHeuristic(root.board)
+    root.gn = 0
+
+    heapq.heappush(fringe, root)
+    while len(fringe) > 0:
+        print("Visited: " + str(len(visited)) +
+              ", Fringe: " + str(len(fringe)))
+
+        currentNode = heapq.heappop(fringe)
+
         visited.append(currentNode)
-        hn = calculateHeuristic(currentNode)
+
+        drawBoard(currentNode.board)
+        print()
 
         # TODO: Add note
-        if hn == 0:
+        if currentNode.hn == 0:
             # Found board
             # TODO: Print board
+            print("Found solution.")
             break
+
+        childrenNodes = []
+        for i in range(N):
+            for j in range(N):
+                if currentNode.board[i][j] == 0:
+                    continue
+                childrenNodes = list(
+                    chain(childrenNodes, generateNextStatesForQueenAt(i, j, currentNode.board)))
+
+        for childNode in childrenNodes:
+            childNode.hn = calculateHeuristic(childNode.board)
+            childNode.gn = currentNode.gn + 1
+
+            isVisitedBefore = False
+            isFringedBefore = False
+            childNodeIsBetter = False
+
+            for visitedNode in visited:
+                if functools.reduce(lambda i, j: i and j, map(lambda m, k: m == k, visitedNode.board, childNode.board), True):
+                    isVisitedBefore = True
+                    break
+
+            for i in range(len(fringe)):
+                if functools.reduce(lambda i, j: i and j, map(lambda m, k: m == k, fringe[i].board, childNode.board), True):
+                    isFringedBefore = True
+                    childNodeIsBetter = childNode.fn() < fringe[i].fn()
+                    del fringe[i]
+                    heapq.heapify(fringe)
+                    break
+
+            if not isVisitedBefore and not isFringedBefore:
+                heapq.heappush(fringe, childNode)
+            elif isFringedBefore and childNodeIsBetter:
+                heapq.heappush(fringe, childNode)
 
 
 board = generateBoard(N)
